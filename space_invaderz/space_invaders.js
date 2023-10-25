f9e3d448d5e14717c3ecd5f1c0bce3adf258d0c1
maherhasannn <99282959+maherhasannn@users.noreply.github.com>
2023-10-25 00:48:15 -0700

    Delete space_invaderz directory

--- space_invaderz/space_invaders.js
+++
@@ -1,9 +0,0 @@
-const backgroundMusic = new Audio("background.wav");
-
-function startGame() {
-  backgroundMusic.play();
-  // Other game initialization code here
-}
-
-// Add a button to your HTML and attach the click event to start the game
-document.getElementById("startButton").addEventListener("click", startGame);
